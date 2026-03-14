f952e889fe00923c2d7e70ac0dae61cd8d61a9c9
Alyson Jean <alyson-jean@hotmail.com>
2026-03-14 08:40:19 -0300

    fix(renderer): envolve PageShell com ErrorBoundary para evitar tela branca em erros de render

--- renderer/PageShell.tsx
+++ renderer/PageShell.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { PageContextProvider } from './usePageContext'
 import type { PageContext } from 'vike/types'
 import { Providers } from '../src/Providers'
+import { ErrorBoundary } from '../src/components/ErrorBoundary'
 
 export function PageShell({ pageContext, children }: { pageContext: PageContext; children: React.ReactNode }) {
   return (
     <React.StrictMode>
       <PageContextProvider pageContext={pageContext}>
         <Providers>
-          {children}
+          <ErrorBoundary>
+            {children}
+          </ErrorBoundary>
         </Providers>
       </PageContextProvider>
     </React.StrictMode>
